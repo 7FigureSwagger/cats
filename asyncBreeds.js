e0a6ee804469315cf1c409b18759b9a074877e26
7 <swistek.labs@gmail.com>
2019-12-04 02:19:26 +0000

    Cat Breeds

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,17 +1,20 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+const breedDetailsFromFile = function(breed, func) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     // ISSUE: Returning from inner callback function, not our main function.
     console.log('Callback: I have the data!')
-    if (!error) return data; 
+
+    if (!error) return func(data); 
   });
-  // ISSUE: Attempting to return data out here will also not work.
+  // ISSUE: Attempting to return data out here will axslso not work.
   //        Currently not returning anything from here, so this function returns undefined.
 }
 
 // we try to get the return value
-const bombay = breedDetailsFromFile('Bombay');
-console.log('Return Value: ', bombay) // => will NOT print out details, instead we will see undefined!
+const bombay = breedDetailsFromFile('Balinese', function(bombay){
+  console.log('Return breed: ', bombay);
+});
+// process.stdout.write('Return Value: ', bombay)
